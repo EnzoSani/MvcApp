aff05ee832566db9cc047560536d1e8c15635473
EnzoSani <enzosanabria23@gmail.com>
2022-05-11 01:50:15 -0300

    Agregar archivos de proyecto.

---
+++ FirstMvcApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMvcApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is the index page!";
+        }
+        public string Hello()
+        {
+            return "Who is there?";
+        }
+    }
+}
